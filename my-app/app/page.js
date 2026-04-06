736963ad0b67a7037e54e0d9805e0795a947a33e
isnika <Ngkhanhhuyen67@gmail.com>
2026-04-06 12:14:20 +0700

    feat: fetch products from API and display product list

---
+++ my-app/app/page.js
@@ -0,0 +1,20 @@
+import ProductCard from "../components/ProductCard";
+
+async function getProducts() {
+  const res = await fetch("https://fakestoreapi.com/products", {
+    cache: "no-store", // tránh cache khi dev
+  });
+  return res.json();
+}
+
+export default async function HomePage() {
+  const products = await getProducts();
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
+      {products.map((item) => (
+        <ProductCard key={item.id} product={item} />
+      ))}
+    </div>
+  );
+}
